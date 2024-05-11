8946901caab8407d5af63280e058823c8af96979
kimhyunjumg <guswjddudwo@gmail.com>
2024-05-11 17:18:41 +0900

    알고리즘 공부

---
+++ 복습/test.mjs
@@ -0,0 +1,26 @@
+let str = "helllow world";
+
+console.log(str);
+
+class node {
+  constructor(data, next = ull) {
+    this.data = data;
+    this, (next = next);
+  }
+}
+
+class linkedList {
+  constructor() {
+    this.head = null;
+    this.count = 0;
+  }
+}
+
+// insertAt(index,data){
+//     if(index > this.count || index <0){
+//         throw new Error('범위를 넘어갔습니다.')
+//     }
+//     let newNode = new Node(data);
+// };
+
+// export {node};
